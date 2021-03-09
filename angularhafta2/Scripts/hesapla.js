5dba1bd3d14ed5e7416ee4cf287897d6fc8409a4
farukarslan <o.farukarslan0@gmail.com>
2021-03-09 21:11:46 +0300

    Add project files.

---
+++ angularhafta2/Scripts/hesapla.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="D:\angularhafta2\angularhafta2\Scripts/angular.js" />
+var uygulamam = angular.module("hesapmodule", []);
+var kontrolur = function ($scope) {
+
+    $scope.sayi1 = 0;
+    $scope.sayi2 = 0;
+    $scope.sonuc = 0;
+
+    $scope.topla = function () {
+        $scope.sonuc = $scope.sayi2 + $scope.sayi1;
+    };
+
+    $scope.carp = function () {
+        $scope.sonuc = $scope.sayi2 * $scope.sayi1;
+    };
+};
+uygulamam.controller("kontrolur", kontrolur);
---
+++ angularhafta2/Scripts/repeatScript.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="angular.js" />
+var uygulamam = angular.module("repeatmodule", []);
+var kontrolur = function ($scope) {
+
+    var isciler = [
+        
+        { name: "aliaaaaaaaa", soyname: "çakır", maas: 1300, cinsiyete: "e" },
+        { name: "betüş", soyname: "arslan", maas: 1400, cinsiyete: "k" },
+        { name: "eçüş", soyname: "saygın", maas: 1900, cinsiyete: "k" },
+        { name: "büçüş", soyname: "öger", maas: 3300, cinsiyete: "e" }
+
+    ];
+    $scope.isciler = isciler;
+    $scope.satirsayisi = 3;
+
+};
+
+
+uygulamam.controller("kontrolur", kontrolur);
